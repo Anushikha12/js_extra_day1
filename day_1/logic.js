2948e28e5e2f0dcf9825dd29484348d14f5b83c9
Anushikha12 <100060666+Anushikha12@users.noreply.github.com>
2022-08-02 15:05:27 +0530

    Add files via upload

---
+++ day_1/logic.js
@@ -0,0 +1,30 @@
+//What is javascript?
+
+//javascript is the programming language that adds functionality to web pages or make webpages dynamic.user starts communicating with the web ages through javascript.
+
+
+// what is the javascript run time environment?
+// The JavaScript runtime environment provides access to built-in libraries and objects that are available to a program so that it can interact with the outside world and make the code work.
+// In the context of a browser comprised of
+// The JavaScript engine (which in turn is made up of the heap and the call stack)
+// Web APIs
+// The callback queue
+// The event loop
+
+//what is execution context?
+//when the js engine executes the js code,it creates an environment called the execution context.it is basically the concept for describing internal working of a code.
+//during execution context, a js code gets parsed line by line and variables and functions are stored in the memory.
+//each execution context has 2 phase-
+//1.)Creation phase
+//2.)Execution phase
+
+//what is javascript Engine?
+// The JavaScript Engine is a program whose responsibility is to execute JavaScript code.
+// different browsers use diffrent js engines to better optimize the output.
+// chrome-v8 engine
+//mozilla firefox-spider monkey
+//internet explorer-chakra
+
+//flow of data in engine?
+// The environment needs to have an engine, which takes the JS code that's written in human-readable syntax and turns it into machine code.
+// The engine uses a parser to go through the code line by line and check if the syntax is correct. If there are any errors, code will stop executing and an error will be thrown.
